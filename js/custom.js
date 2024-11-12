b79e309061888065ffbcf8f35c601be28c6e47a4
ajdorfman93 <dorfmanart@gmail.com>
2024-11-12 15:39:51 -0500

    Upload Website

---
+++ js/custom.js
@@ -0,0 +1,353 @@
+/** 
+  * Template Name: Home Property
+  * Version: 1.0  
+  * Template Scripts
+  * Author: MarkUps
+  * Author URI: http://www.markups.io/
+
+  Custom JS
+  
+
+  1. FIXED NAVBAR 
+  2. AGENTS SLIDER
+  3. TESTIMONIAL SLIDER  
+  4. CLIENT BRAND SLIDER (SLICK SLIDER)
+  5. TOP SLIDER (SLICK SLIDER)
+  6. LATEST PRODUCT SLIDER (SLICK SLIDER)
+  7. HOVER DROPDOWN MENU
+  8. ADVANCE SEARCH FILTER  (noUiSlider SLIDER)
+  9. MIXIT FILTER ( FOR GALLERY ) 
+  10. FANCYBOX ( FOR PORTFOLIO POPUP VIEW )
+  11. SCROLL TOP BUTTON
+  12. PRELOADER
+  13. GRID AND LIST LAYOUT CHANGER 
+  14.RELATED ITEM SLIDER (SLICK SLIDER)
+
+  
+**/
+
+jQuery(function($){
+
+
+  /* ----------------------------------------------------------- */
+  /*  1. FIXED NAVBAR 
+  /* ----------------------------------------------------------- */
+    
+    
+  jQuery(window).bind('scroll', function () {
+    if (jQuery(window).scrollTop() > 200) {
+        jQuery('.main-navbar').addClass('navbar-fixed-top');
+        
+      } else {
+          jQuery('.main-navbar').removeClass('navbar-fixed-top');          
+      }
+  });
+  
+  /* ----------------------------------------------------------- */
+  /*  2. AGENTS SLIDER
+  /* ----------------------------------------------------------- */    
+
+    jQuery('.aa-agents-slider').slick({
+      dots: false,
+      arrows: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+  /* ----------------------------------------------------------- */
+  /*  3. TESTIMONIAL SLIDER 
+  /* ----------------------------------------------------------- */    
+
+    jQuery('.aa-testimonial-slider').slick({
+        dots: false,      
+        infinite: true,
+        speed: 500,      
+        cssEase: 'linear'
+      });
+
+  /* ----------------------------------------------------------- */
+  /*  4. CLIENT BRAND SLIDER (SLICK SLIDER)
+  /* ----------------------------------------------------------- */  
+
+   jQuery('.aa-client-brand-slider').slick({
+      dots: false,
+      arrows: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+  
+  /* ----------------------------------------------------------- */
+  /*  5. TOP SLIDER (SLICK SLIDER)
+  /* ----------------------------------------------------------- */    
+
+    jQuery('.aa-top-slider').slick({
+      dots: false,
+      infinite: true,
+      arrows: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    });
+    
+  /* ----------------------------------------------------------- */
+  /*  6. LATEST PRODUCT SLIDER (SLICK SLIDER)
+  /* ----------------------------------------------------------- */      
+
+    jQuery('.aa-properties-details-img').slick({
+      dots: false,
+      infinite: true,
+      arrows: true,
+      speed: 500,      
+      cssEase: 'linear'
+    });
+
+      
+  /* ----------------------------------------------------------- */
+  /*  7. HOVER DROPDOWN MENU
+  /* ----------------------------------------------------------- */ 
+  
+  // for hover dropdown menu
+    jQuery('ul.nav li.dropdown').hover(function() {
+      jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+    }, function() {
+      jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+    });
+
+ 
+ /* ----------------------------------------------------------- */
+/* 8. ADVANCE SEARCH FILTER  (noUiSlider SLIDER)
+/* ----------------------------------------------------------- */        
+jQuery(function() {
+  if (jQuery('body').is('.aa-price-range')) {
+    // Original range values
+    var originalRange = {
+      'min': 0,
+      '10%': 2000000,
+      '20%': 5000000,
+      '30%': 10000000,
+      '40%': 15000000,
+      '50%': 20000000,
+      '60%': 25000000,
+      '70%': 30000000,
+      '80%': 35000000,
+      '90%': 40000000,
+      'max': 50000000
+    };
+
+    // Alternative range for "For Rent" option
+    var rentRange = {
+      'min': 0,
+      '10%': 500000,
+      '20%': 1000000,
+      '30%': 2000000,
+      '40%': 3000000,
+      '50%': 4000000,
+      '60%': 5000000,
+      '70%': 6000000,
+      '80%': 7000000,
+      '90%': 8000000,
+      'max': 10000000
+    };
+
+    // Initialize slider with original range
+    var skipSlider = document.getElementById('aa-sqrfeet-range');
+    var sliderConfig = {
+      range: originalRange,
+      snap: true,
+      connect: true,
+      start: [0, 5000000]
+    };
+    noUiSlider.create(skipSlider, sliderConfig);
+
+    // Display currency values with formatting
+    var skipValues = [
+      document.getElementById('skip-value-lower'),
+      document.getElementById('skip-value-upper')
+    ];
+    skipSlider.noUiSlider.on('update', function(values, handle) {
+      skipValues[handle].innerHTML = '₪' + parseInt(values[handle]).toLocaleString();
+    });
+
+    // Listen for changes on the Property Status dropdown
+    document.getElementById('property-status').addEventListener('change', function() {
+      var selectedValue = this.value;
+      var newRange = selectedValue === "2" ? rentRange : originalRange; // Use rentRange if "For Rent" is selected
+
+      // Update slider with the new range
+      skipSlider.noUiSlider.updateOptions({
+        range: newRange
+      });
+    });
+  }
+});
+
+  /* ----------------------------------------------------------- */
+  /*  9. MIXIT FILTER ( FOR GALLERY ) 
+  /* ----------------------------------------------------------- */  
+
+    jQuery(function(){
+      jQuery('#mixit-container').mixItUp();
+    });
+
+  /* ----------------------------------------------------------- */
+  /*  10. FANCYBOX ( FOR PORTFOLIO POPUP VIEW ) 
+  /* ----------------------------------------------------------- */ 
+      
+    jQuery(document).ready(function() {
+      jQuery(".fancybox").fancybox();
+    });  
+   
+    
+  /* ----------------------------------------------------------- */
+  /*  11. SCROLL TOP BUTTON
+  /* ----------------------------------------------------------- */
+
+  //Check to see if the window is top if not then display button
+
+    jQuery(window).scroll(function(){
+      if (jQuery(this).scrollTop() > 300) {
+        jQuery('.scrollToTop').fadeIn();
+      } else {
+        jQuery('.scrollToTop').fadeOut();
+      }
+    });
+     
+    //Click event to scroll to top
+
+    jQuery('.scrollToTop').click(function(){
+      jQuery('html, body').animate({scrollTop : 0},800);
+      return false;
+    });
+  
+  /* ----------------------------------------------------------- */
+  /*  12. PRELOADER
+  /* ----------------------------------------------------------- */
+
+   jQuery(window).load(function() { // makes sure the whole site is loaded      
+      jQuery('#aa-preloader-area').delay(300).fadeOut('slow'); // will fade out      
+    })
+   
+
+
+  /* ----------------------------------------------------------- */
+  /*  13. GRID AND LIST LAYOUT CHANGER 
+  /* ----------------------------------------------------------- */
+
+  jQuery("#aa-list-properties").click(function(e){
+    e.preventDefault(e);
+    jQuery(".aa-properties-nav").addClass("aa-list-view");
+  });
+  jQuery("#aa-grid-properties").click(function(e){
+    e.preventDefault(e);
+    jQuery(".aa-properties-nav").removeClass("aa-list-view");
+  });
+
+
+  /* ----------------------------------------------------------- */
+  /*  14. RELATED ITEM SLIDER (SLICK SLIDER)
+  /* ----------------------------------------------------------- */      
+
+    jQuery('.aa-related-item-slider').slick({
+      dots: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    }); 
+
+ 
+});
+
